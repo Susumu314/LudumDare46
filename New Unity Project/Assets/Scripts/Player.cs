6542acfcbb24dda453448fbbc0416b735b4c39da
Hugo Susumu Mota Asaga <hugo.asaga@usp.br>
2020-04-18 10:02:47 -0300

    First commit
    MovePlayer

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float gravity = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move(){
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newPosX = transform.position.x + deltaX;
+        var deltaZ = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newPosZ = transform.position.z + deltaZ;
+        transform.position = new Vector3(newPosX, transform.position.y, newPosZ);
+    }
+}
